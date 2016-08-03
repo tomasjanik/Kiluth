43bf0fc43c3c5ff2092bd5dfcbcd07491f4e4fa2
poompengcharoen <poom.pengcharoen@gmail.com>
2016-08-03 11:02:28 +0700

    Marketing update

---
+++ marketing/ads/1-hello/development/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Hello.</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
+    <style>
+        <?php require_once('fonts/theinhardt.css'); ?>
+        <?php require_once('main.css');?> 
+    </style>
+</head>
+
+<body>
+    <div class="email-background">
+        <div class="email-container">
+            <?php require_once('content.php'); ?>
+                <?php require_once('footer.php'); ?>
+        </div>
+    </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+</body>
+</html>
