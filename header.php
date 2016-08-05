b02a2815d9b34810b45b32b5f6b5e9bfd72e9935
poompengcharoen <poom.pengcharoen@gmail.com>
2016-08-06 00:38:26 +0700

    header.php

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
         </div>
         <ul class="nav navbar-nav desktop-only">
             <?php 
+            session_start();
             $pageurl = $_SERVER['REQUEST_URI'];
 //            echo $pageurl;
             
@@ -18,36 +19,50 @@
             $underline3 = 'muted'; //logbook
             $underline4 = 'muted'; //contact
             $underline5 = 'muted'; //admin
+            $underline6 = 'muted'; //library
+            $underline7 = 'muted'; //library
+            $underline8 = 'muted'; //database
             
             if (strpos($pageurl, 'projects') !== false) {
-                $underline1 = 'unmuted'; //projects
-                
+                $underline1 = 'unmuted';
             }
             elseif (strpos($pageurl, 'case-study') !== false) {
-                $underline1 = 'unmuted'; //case-study
+                $underline1 = 'unmuted';
                 
             }
             elseif (strpos($pageurl, 'about') !== false) {
-                $underline2 = 'unmuted'; //about
+                $underline2 = 'unmuted';
             }
             elseif (strpos($pageurl, 'logbook') !== false) {
-                $underline3 = 'unmuted'; //logbook
+                $underline3 = 'unmuted';
             }
             elseif (strpos($pageurl, 'log.php') !== false) {
-                $underline3 = 'unmuted'; //log
+                $underline3 = 'unmuted';
             }
             elseif (strpos($pageurl, 'contact') !== false) {
-                $underline4 = 'unmuted'; //contact
+                $underline4 = 'unmuted';
             }
-            elseif (strpos($pageurl, 'admin.php') !== false) {
-                $underline5 = 'unmuted'; //admin
+            elseif (strpos($pageurl, 'admin') !== false) {
+                $underline5 = 'unmuted';
+            }
+            elseif (strpos($pageurl, 'library') !== false) {
+                $underline6 = 'unmuted';
+            }
+            elseif (strpos($pageurl, 'generator') !== false) {
+                $underline7 = 'unmuted';
+            }
+            elseif (strpos($pageurl, 'generator') !== false) {
+                $underline8 = 'unmuted';
             }
             else {
                 $underline1 = 'unmuted'; //projects
                 $underline2 = 'unmuted'; //about
                 $underline3 = 'unmuted'; //logbook
                 $underline4 = 'unmuted'; //contact
-                $underline5 = 'unmuted'; //contact
+                $underline5 = 'unmuted'; //admin
+                $underline6 = 'unmuted'; //library
+                $underline7 = 'unmuted'; //generator
+                $underline8 = 'unmuted'; //database
             }
             
             ?>
@@ -56,12 +71,13 @@
                 <li><a class="link-ani <?php echo $underline3; ?>" href="logbook.php">Logbook</a></li>
                 <li><a class="link-ani <?php echo $underline4; ?>" href="contact.php">Contact</a></li>
                 <?php
-            if (strpos($pageurl, 'admin') !== false) {
+            if ($_SESSION['admin'] == "yes") {
                 ?>
                     <li><a class="link-ani <?php echo $underline5; ?>" href="admin.php">Admin</a></li>
-                    <?php
-            }
-            ?>
+                    <li><a class="link-ani <?php echo $underline6; ?>" href="library.php">Library</a></li>
+                    <li><a class="link-ani <?php echo $underline7; ?>" href="generator.php">Generate Invoice</a></li>
+                    <li><a class="link-ani <?php echo $underline8; ?>" href="database">Database</a></li>
+                <?php } ?>
         </ul>
 
         <ul class="mobile-only nav navbar-nav mobile-nav" hidden="hidden">
